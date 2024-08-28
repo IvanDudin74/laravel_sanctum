e05dc30d71452b29d2d2ccf609cc6d37965af545
Иван <info-memo74@mail.ru>
2024-08-28 23:34:03 +0500

    Commit 2

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -38,10 +38,10 @@
 <template>
     <div>
         This is IndexComponent<br><br>
-        <router-link v-if="!authentificated" :to="{ name: 'user.registration'}">Registration</router-link><br><br>
-        <router-link v-if="!authentificated" :to="{ name: 'user.login'}">Login</router-link><br><br>
-        <router-link v-if="authentificated" :to="{ name: 'user.personal'}">Personal</router-link><br><br>
-        <router-link v-if="authentificated" :to="{ name: 'get.index'}">Get</router-link><br><br>
+        <router-link v-if="!authentificated" :to="{ name: 'user.registration'}">Registration</router-link><br>
+        <router-link v-if="!authentificated" :to="{ name: 'user.login'}">Login</router-link><br>
+        <router-link v-if="authentificated" :to="{ name: 'user.personal'}">Personal</router-link><br>
+        <router-link v-if="authentificated" :to="{ name: 'get.index'}">Get</router-link><br>
         <a v-if="authentificated" href="#" @click.prevent="logout()">Logout</a><br><br>
         <router-view></router-view>
     </div>

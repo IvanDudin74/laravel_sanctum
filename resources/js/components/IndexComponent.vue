04c9e2f5ddb6b0da063fd29b34a2d8485ef35b39
User-PC <PROFESSIONAL>
2024-08-30 14:58:52 +0500

    Commit 1

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -12,6 +12,7 @@
             logout() {
                 axios.post('/logout')
                     .then(res => {
+                        console.log(111111111111111111);
                         localStorage.removeItem('authentificated')
                         this.$router.push({ name: 'user.login'})
                     })
@@ -39,7 +40,7 @@
     <div>
         This is IndexComponent<br><br>
         <router-link v-if="!authentificated" :to="{ name: 'user.registration'}">Registration</router-link><br>
-        <router-link v-if="!authentificated" :to="{ name: 'user.login'}">Login</router-link><br>
+        <router-link :to="{ name: 'user.login'}">Login</router-link><br>
         <router-link v-if="authentificated" :to="{ name: 'user.personal'}">Personal</router-link><br>
         <router-link v-if="authentificated" :to="{ name: 'get.index'}">Get</router-link><br>
         <a v-if="authentificated" href="#" @click.prevent="logout()">Logout</a><br><br>
